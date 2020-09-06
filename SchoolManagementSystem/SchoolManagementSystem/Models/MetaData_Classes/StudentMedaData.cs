0e671e6709c813fbe1e10e9d5c4f552a27846420
Claudia Ziemba <claudia.ziemba@gmail.com>
2020-09-06 20:51:02 -0300

    Models update

---
+++ SchoolManagementSystem/SchoolManagementSystem/Models/MetaData_Classes/StudentMedaData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagementSystem.Models
+{
+    public class StudentMedaData
+    {
+        
+        [StringLength(50)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [StringLength(50)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Display(Name = "Enrollment Date")]
+        public Nullable<System.DateTime> EnrollmentDate { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+    }
+    [MetadataType(typeof(StudentMedaData))]
+    public partial class Student
+    {
+        
+    }
+}
